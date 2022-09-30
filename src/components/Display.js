ec627ce69de74c553c7f77a1bf5de0686e1dc31a
BahirathyR <rbahirathy@gmail.com>
2022-09-30 06:42:49 +0530

    List Display

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,4 +1,4 @@
 
 export const borderInit = '2px solid #00BFFF'
 export const borderChange = '2px solid green'
-export const headInit={background:'blue',width:'100%',height:'30%',marginTop:'0%',color:'white',textAlign:'center'}
+export const headInit = { background: 'blue', width: '100%', height: '10%', marginTop: '0%', color: 'white', textAlign: 'center'}
--- src/components/DisplayList.js
+++ src/components/DisplayList.js
@@ -1,19 +1,27 @@
 import React, { useState,useEffect } from 'react'
 import '../App.css'
-import { borderChange,borderInit, headInit } from './Display'
+import { borderChange, borderInit, headInit } from './Display'
+const arr=[]
 const DisplayList = () => {
   const [data, setData] = useState([])
+  const [resdata,setResdata]=useState([])
   const [btcolor, setBtcolor] = useState('#1E90FF')
   const [borderColor, setBorderColor] = useState(borderInit)
   const[header,setHeader]=useState('#00BFFF')
-  console.log(data.fact)
+  console.log("data",data)
   useEffect(() => {
     fetchData();
   }, [])
   const fetchData = async() => {
     const response = await fetch('https://catfact.ninja/fact')
-      .then(response => { return response.json()})
-    setData(response)
+      .then(response => { return response.json() })
+      const res = response.fact
+    setData([...data, res])
+    const count = data.filter((item) => item).length
+    console.log("count",count)
+   if(count < 10){
+      setResdata([...resdata,res])
+    }
   }
   const changeColor = () => {
     btcolor === '#1E90FF' ? setBtcolor('#2E8B57') : setBtcolor('#1E90FF')
@@ -27,9 +35,11 @@ const DisplayList = () => {
   return (
     <div className='container' style={{border: borderColor}}>
       <h1 style={{ ...headInit, background: header }}>List of Facts</h1>
-      <ul className='a'>
-        <li className='li'>{data.fact}</li>
+      {resdata.map((item) =>
+        <ul className='a'>
+          <li className='li'>{item}</li>
         </ul>
+  )}
       <div className='center'>
         <button className='button' style={{ background: btcolor }} onClick={() => { fetchData(); changeColor(); changeBorder(); headerChange()}}>Click Me</button>
         </div>
